6fad4bbc475d988f05e86d1b88de48d9a03b5f59
muralikatta12 <muralisaveetha7@gmail.com>
2025-03-03 11:32:58 +0530

    change product name

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const Product = require('../models/product');
+const Product = require('../models/Product');
 const Firm = require('../models/Firm');
 
 const storage = multer.diskStorage({
@@ -19,8 +19,8 @@ const addProduct = async (req, res) => {
         const { productName, price, category, bestseller, description } = req.body;
         const firmId = req.params.firmId;
 
-        // Convert category to array if it's not already
-        const categoryArray = Array.isArray(category) ? category : [category];
+        // Convert category to an array if it's not already
+        const categoryArray = category ? (Array.isArray(category) ? category : [category]) : [];
 
         // Convert bestseller to Boolean
         const bestsellerBoolean = bestseller === 'true' || bestseller === true;
@@ -64,23 +64,25 @@ const getproductByFirm = async (req, res) => {
         }
 
         const products = await Product.find({ firm: firmId });
-        res.status(200).json({restaurantName,products});
+        res.status(200).json({ firmName: firm.firmName, products });
     } catch (error) {
         console.error("Error in getproductByFirm:", error);
         res.status(500).json({ error: "Internal server error" });
     }
 };
 
-const deleteProductById = async (req,res)=>{
-    try{
+const deleteProductById = async (req, res) => {
+    try {
         const productId = req.params.productId;
-        const deletedProduct  = await Product.findByIdAndDelete(productId);
-        if(!deletedProduct){
-            return res.status(404).json({error:"No product found"});
+        const deletedProduct = await Product.findByIdAndDelete(productId);
+        if (!deletedProduct) {
+            return res.status(404).json({ error: "No product found" });
         }
-    }catch(error){
-        console.error("Error in deleteProduct:",error);
-        res.status(500).json({error:"Internal server error"});
+        res.status(200).json({ message: "Product deleted successfully" });
+    } catch (error) {
+        console.error("Error in deleteProduct:", error);
+        res.status(500).json({ error: "Internal server error" });
     }
-}
+};
+
 module.exports = { addProduct: [upload.single('image'), addProduct], getproductByFirm, deleteProductById };
