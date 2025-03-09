ac3881705bb24ab7cd9563c3bc09acc0f67c9d68
muralikatta12 <muralisaveetha7@gmail.com>
2025-03-09 10:50:13 +0530

    change

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -6,29 +6,37 @@ dotEnv.config()
 
 const secretKey = process.env.SECRET_KEY;
 
-
-const verifyToken = async(req, res, next) => {
-    const token = req.headers.token;
-
-    if (!token) {
-        return res.status(401).json({ error: "Token is required" });
-    }
+const verifyToken = async (req, res, next) => {
     try {
-        const decoded = jwt.verify(token, secretKey)
+        // Check if the Authorization header exists
+        if (!req.headers.authorization) {
+            return res.status(401).json({ error: "Authorization header is missing" });
+        }
+
+        // Split the token
+        const loginToken = req.headers.authorization.split(' ')[1];
+
+        if (!loginToken) {
+            return res.status(401).json({ error: "Token is required" });
+        }
+        //verifying   the token
+        const decoded = jwt.verify(loginToken, secretKey);
         const vendor = await Vendor.findById(decoded.vendorId);
 
         if (!vendor) {
-            return res.status(404).json({ error: "vendor not found" })
+            return res.status(404).json({ error: "Vendor not found" });
         }
 
-        req.vendorId = vendor._id
+        // Attach vendor ID to the request object
+        req.vendorId = vendor._id;
 
-        next()
+        next();
     } catch (error) {
-        console.error(error)
-        return res.status(500).json({ error: "Invalid token" });
-    }
 
+        
+        return res.status(401).json({ error: "Invalid or expired token" });
+    }
 }
 
-module.exports = verifyToken
+module.exports = verifyToken;
+///exporting the verifyToken middleware
